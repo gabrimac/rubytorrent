6628a71fcd6b691a95ffa4f2d090cfb5e761fee5
Gabriel Machuca <gabri.mac@gmail.com>
2014-11-02 13:06:38 +0100

    remove jbuilder and databases

--- app.rb
+++ app.rb
@@ -3,25 +3,24 @@ require 'multi_json'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'pry-byebug'
-require 'jbuilder'
 
 class Rubytorrent < Sinatra::Application
   enable :sessions
 
   configure :development do
-    set :database, "sqlite3:////tmp/my.db"
-    if (RUBY_PLATFORM == "java")
-      require 'jdbc/sqlite3'
-      Jdbc::SQLite3.load_driver
-    end
+    # set :database, "sqlite3:////tmp/my.db"
+    # if (RUBY_PLATFORM == "java")
+    #   require 'jdbc/sqlite3'
+    #   Jdbc::SQLite3.load_driver
+    # end
   end
 
   configure :production do
-    set :database, 'postgres://postgres:12345@localhost/sinatra_service'
-    if (RUBY_PLATFORM == "java")
-      require 'jdbc/postgres'
-      Jdbc::Postgres.load_driver
-    end
+    # set :database, 'postgres://postgres:12345@localhost/sinatra_service'
+    # if (RUBY_PLATFORM == "java")
+    #   require 'jdbc/postgres'
+    #   Jdbc::Postgres.load_driver
+    # end
   end
 end
 
